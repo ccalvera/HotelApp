83edd67151fd3235d6ce26f0f5321ee98619a362
d.calvera@utp.edu.co <d.calvera@utp.edu.co>
2023-05-17 10:29:48 -0500

    feat: enviroment

--- enviroments/enviroment.ts
+++ enviroments/enviroment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  LOCAL: 'http://localhost:3000/',
   API: ' https://my-json-server.typicode.com/ccalvera/hoteldb/',
   SERVER: 'http://20.172.149.117:3000/',
 };

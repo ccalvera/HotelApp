dc7b8610a4ad0f38341f81fe276544ffd07b9bad
d.calvera@utp.edu.co <d.calvera@utp.edu.co>
2023-05-17 00:39:58 -0500

    fix: reservation style

--- enviroments/enviroment.prod.ts
+++ enviroments/enviroment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   API: 'http://localhost:3000/',
+  SERVER: 'http://20.172.149.117:3000/',
 };
--- enviroments/enviroment.ts
+++ enviroments/enviroment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  API: 'http://localhost:3000/',
+  API: ' https://my-json-server.typicode.com/ccalvera/hoteldb/',
+  SERVER: 'http://20.172.149.117:3000/',
 };
